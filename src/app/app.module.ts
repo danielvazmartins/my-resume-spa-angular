285bef80930521a49fac08c55b606fa0dd22dea2
Daniel Vaz Villalobos Martins <danielvazmartins@gmail.com>
2024-04-12 09:48:32 -0300

    finalizando migração da home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { HomeComponent } from "./home.component";
+import { FooterModule } from "../shared/components/footer/footer.module";
 
 @NgModule({
     declarations: [
         HomeComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        FooterModule
     ],
     exports: [
         HomeComponent
